10774b74c7c15c1ab32a08e685908ac358a5790d
DIDAC-PC\DIDAC <didivg2005@gmail.com>
2024-01-02 19:02:00 +0100

    2/1

--- Assets/Creditos.cs
+++ Assets/Creditos.cs
@@ -3,30 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class Creditos : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
-    public void EscenaJuego()
+    public void MenuPrincipal()
     {
-        SceneManager.LoadScene("Juego");
-    }
-    public void Salir()
-    {
-        Application.Quit();
-    }
-    public void Creditos()
-    {
-        SceneManager.LoadScene("Créditos");
+        SceneManager.LoadScene("Menu");
     }
 }
