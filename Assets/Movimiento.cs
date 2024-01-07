8273a54860fffb8b94a8c31c428fa3bfd58621bc
DIDAC-PC\DIDAC <didivg2005@gmail.com>
2024-01-07 23:20:12 +0100

    SK-808 final

--- Assets/Movimiento.cs
+++ Assets/Movimiento.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public enum Velocidades {Quieto = 0, Lento = 1, Normal = 2, Rapido = 3 };
+public enum Velocidades {Quieto = 0, Plataforma = 1, Lento = 2, Normal = 3, Rapido = 4};
 public class Movimiento : MonoBehaviour
 {
     public Velocidades VelocidadActual;
-    float[] Velocidad = {0f, 8.6f, 10.4f, 12.96f};
+    float[] Velocidad = { 0f, 8f, 8.6f, 10.4f, 12.96f };
     // Start is called before the first frame update
     public Transform GroundCheckTransform;
     public float GroundCheckRadius;
@@ -15,6 +16,7 @@ public class Movimiento : MonoBehaviour
     public int Saltos;
     public float Gravedad = 12.41067f;
     public float FuerzaSalto = 26.6581f;
+    public bool Checkpoint = false;
 
     Rigidbody2D rb;
     void Start()
@@ -22,7 +24,7 @@ public class Movimiento : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -42,7 +44,7 @@ public class Movimiento : MonoBehaviour
             }
             else
             {
-                if(Saltos < 1)
+                if (Saltos < 1)
                 {
                     Saltos += 1;
                     rb.velocity = Vector2.zero;
@@ -56,6 +58,36 @@ public class Movimiento : MonoBehaviour
     {
         Grounded = true;
         Saltos = 0;
+        if (collision.gameObject.tag == "Finish")
+        {
+            transform.position += Vector3.right * 0 * Time.deltaTime;
+        }
+        if (collision.gameObject.tag == "Respawn")
+        {
+            Checkpoint = true;
+        }
+        if (collision.gameObject.tag == "Pincho")
+        {
+            if (Checkpoint == true)
+            {
+                SceneManager.LoadScene("Juego 1");
+            }
+            else
+            {
+                SceneManager.LoadScene("Juego");
+            }
+        }
+        if (collision.gameObject.tag == "Vacio")
+        {
+            if (Checkpoint == true)
+            {
+                SceneManager.LoadScene("Juego 1");
+            }
+            else
+            {
+                SceneManager.LoadScene("Juego");
+            }
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
